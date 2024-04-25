6e7b07fc0d049710eff64dfa1f78e8b54d716a9c
Anastasiya Riabova <vereten.o@yandex.ru>
2024-04-25 16:31:04 +0300

    add app

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -1,4 +1,4 @@
-package ru.netology;
+package ru.netology.testmode.test;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
